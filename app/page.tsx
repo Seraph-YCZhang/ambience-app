6c353b8f74c4d788db8b626c624e97c23c72d6e0
Yuchen Zhang <yuchen.zhang@bytedance.com>
2023-12-11 01:30:50 -0800

    icon and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Footer from '@/components/Footer'
-import Form from '@/components/Form'
-import Header from '@/components/Header'
-import QuickCheck from '@/components/QuickCheck'
-import Image from 'next/image'
+import Footer from '@/components/Footer';
+import Form from '@/components/Form';
+import Header from '@/components/Header';
+import QuickCheck from '@/components/QuickCheck';
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-     <Header />
-     <Form />
-     <QuickCheck />
-     <Footer />
-    </main>
-  )
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between'>
+			<Header />
+			<Form />
+			<QuickCheck />
+			<Footer />
+		</main>
+	);
 }
