9f5986eafa208c31d5a919ef8054b7db0d8fedf2
Yuchen Zhang <yuchen.zhang@bytedance.com>
2023-12-14 19:23:54 -0800

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
+      <Script id="google-tag-manager" strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', "GTM-PRSGPCQH");
+  `,
+        }}
+      />
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
